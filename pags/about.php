a4b4ecf9c1f0e99667fab6e2041d9cb46553a4b0
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2023-07-19 18:09:45 -0300

    About Us - Updates

--- pags/about.php
+++ pags/about.php
@@ -1,8 +1,9 @@
 <?php include("../template-parts/header.php"); ?>
 
 <div class="container">
-<main class="about">
-    <?php include("../template-parts/form-about.php") ?>
-</main>    
+    <?php
+    $aboutID = 1;
+    include("../template-parts/about-content.php")
+    ?>   
 </div>
 <?php include("../template-parts/footer.php"); ?>
